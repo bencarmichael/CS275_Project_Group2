89c98d340c0c1d19a185adb9cd11dde5a3bc6dc0
derek-mallon <derekmallon@tutanota.com>
2017-03-17 13:21:41 -0400

    fixed a small string error

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -130,9 +130,9 @@ function look_up_recipe(){
                 }
                 recipe_result.append(
                     '\
-                    <div id="'+recipe.name+'" server-id="'+recipe.server_id+'" description="''"class="panel panel-default">\
+                    <div id="'+recipe.name+'" server-id="'+recipe.server_id+'" description="'+ recipe.description +'"class="panel panel-default">\
                         <div class="panel-heading"><a href="#" class="panel-title">'+recipe.name+'<span class="badge pull-right">'+recipe.completeness+'%</span></a></div>\
-                        <div class="panel-body">' + ingredient_str '\
+                        <div class="panel-body">' + ingredient_str + '\
                         </div>\
                     </div>\
                     '
